ebb733ff218f569bb44549cd2c4a11565c45d53d
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2023-12-23 21:58:31 +0000

    Get values

--- public/index.js
+++ public/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('test');
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('Stefan', 'work on website', 250);
+const invTwo = new Invoice('Ivan', 'work on CSS', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('test')
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice('Stefan', 'work on website', 250);
+const invTwo = new Invoice('Ivan', 'work on CSS', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
